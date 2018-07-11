1f20fc404406b7942b86201c0146d3175f7a7a09
Robin <rserinel@gmail.com>
2018-07-11 21:01:27 +0200

    Site totally revamped, navbar responsive changed to black. added more images.

--- assets/js/scroll-down.js
+++ assets/js/scroll-down.js
@@ -1,13 +1,19 @@
-$(document).ready(function(){
-    $("a").on('click', function(event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            var hash = this.hash;
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function(){
-                window.location.hash = hash;
-            });
-        }
-    });
+// Scroll to specific values
+// scrollTo is the same
+window.scroll({
+    top: 2500,
+    left: 0,
+    behavior: 'smooth'
+});
+
+// Scroll certain amounts from current position
+window.scrollBy({
+    top: 100, // could be negative value
+    left: 0,
+    behavior: 'smooth'
+});
+
+// Scroll to a certain element
+document.querySelector('.hello').scrollIntoView({
+    behavior: 'smooth'
 });
